5c1fab607fc3a6011f0e10a0fb3d945bb179ac12
Jules-WinnfieldX <61347133+Jules-WinnfieldX@users.noreply.github.com>
2021-07-02 14:28:35 -0700

    Create settings.py
    Adds setting file

---
+++ settings.py
@@ -0,0 +1,7 @@
+# Change at own risk, values here are default
+# Defaults:
+# threads = 0 (Will run all threads), **this is not the same as cores.**
+# download_folder = "./Downloads/" will download in the CyberDropDownloader folder
+
+threads = 0
+download_folder = "./Downloads/"
