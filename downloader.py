08c67e6e4dd515c3d7d9484262eb33aabb9dffc4
Jules-WinnfieldX <61347133+Jules-WinnfieldX@users.noreply.github.com>
2021-07-04 17:16:15 -0700

    Update downloader.py
    Update version number

--- downloader.py
+++ downloader.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 
     response = requests.get("https://api.github.com/repos/Jules-WinnfieldX/CyberDropDownloader/releases/latest")
     latestVersion = response.json()["tag_name"]
-    currentVersion = "1.2.7"
+    currentVersion = "1.2.8"
 
     if latestVersion != currentVersion:
         print("A new version of CyberDropDownloader is available\n"
