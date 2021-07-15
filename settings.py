0f5797750d726dfe76458e0935ef04b288cfbade
Jules-WinnfieldX <61347133+Jules-WinnfieldX@users.noreply.github.com>
2021-07-15 08:42:03 -0700

    Update settings.py
    Added ability to skip files after X tries (setting file)

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@
 # Defaults:
 # threads = 0 (Will run all threads), **this is not the same as cores.**
 # download_folder = "./Downloads/" will download in the CyberDropDownloader folder
+# file_attempts = 0 (will try infinitely), making it 5 as an example will try to download 5 times and then skip.
 
 threads = 0
 download_folder = "./Downloads/"
+file_attempts = 2
