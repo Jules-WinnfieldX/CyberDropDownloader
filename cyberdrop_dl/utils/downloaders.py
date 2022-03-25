b3945355c279f933687cbff3319bab61d91efbf5
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2022-03-25 17:07:34 -0600

    Test

--- cyberdrop_dl/utils/downloaders.py
+++ cyberdrop_dl/utils/downloaders.py
@@ -118,8 +118,7 @@ class Downloader:
             show_progress: bool = True
     ) -> None:
         """Download the content of given URL"""
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                     'Chrome/97.0.4692.71 Safari/537.36'
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
         headers = {'Referer': referral, 'user-agent': user_agent}
         ssl_context = ssl.create_default_context(cafile=certifi.where())
 
