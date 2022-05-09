9eb1915e48ea6191558c80709791627b8d36734e
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2022-05-09 15:50:27 -0600

    read me, alphabetize some stuff.

--- cyberdrop_dl/utils/scraper_helper.py
+++ cyberdrop_dl/utils/scraper_helper.py
@@ -5,16 +5,16 @@ from yarl import URL
 
 from .crawlers.Anonfiles_Spider import AnonfilesCrawler
 from .crawlers.Chibisafe_Spider import ChibisafeCrawler
+from .crawlers.Coomer_Spider import CoomerCrawler
+from .crawlers.Cyberfile_Spider import CyberfileCrawler
 from .crawlers.Erome_Spider import EromeCrawler
+from .crawlers.Gfycat_Spider import GfycatCrawler
 from .crawlers.GoFile_Spider import GofileCrawler
+from .crawlers.Pixeldrain_Crawler import PixelDrainCrawler
+from .crawlers.Redgifs_Spider import RedGifsCrawler
 from .crawlers.ShareX_Spider import ShareXCrawler
 from .crawlers.Thotsbay_Spider import ThotsbayCrawler
-from .crawlers.Gfycat_Spider import GfycatCrawler
-from .crawlers.Redgifs_Spider import RedGifsCrawler
-from .crawlers.Cyberfile_Spider import CyberfileCrawler
-from .crawlers.Coomer_Spider import CoomerCrawler
-from .crawlers.Pixeldrain_Crawler import PixelDrainCrawler
-from .base_functions import log, pixeldrain_parse
+from .base_functions import log
 from .data_classes import CascadeItem
 
 
@@ -25,31 +25,36 @@ class ScrapeMapper():
         self.thotsbay_auth = thotsbay_auth
         self.session = session
         self.Cascade = CascadeItem({})
-        self.erome_crawler = None
-        self.sharex_crawler = None
-        self.chibisafe_crawler = None
-        self.gofile_crawler = None
-        self.anonfiles_crawler = None
-        self.thotsbay_crawler = None
-        self.gfycat_crawler = None
-        self.redgifs_crawler = None
-        self.cyberfile_crawler = None
-        self.coomer_crawler = None
-        self.pixeldrain_crawler = None
-        self.semaphore = asyncio.Semaphore(1)
-        self.mapping = {"pixl.is": self.ShareX, "putme.ga": self.ShareX, "putmega.com": self.ShareX,
-                        "jpg.church": self.ShareX, "cyberdrop.me": self.Chibisafe, "cyberdrop.cc": self.Chibisafe,
-                        "cyberdrop.to": self.Chibisafe, "cyberdrop.nl": self.Chibisafe, "bunkr.is": self.Chibisafe,
-                        "bunkr.to": self.Chibisafe, "erome.com": self.Erome, "gofile.io": self.GoFile,
-                        "anonfiles.com": self.Anonfiles, "pixeldrain.com": self.Pixeldrain,
-                        "thotsbay.com": self.ThotsBay, "socialmediagirls.com": self.ThotsBay,
-                        "gfycat.com": self.gfycat, "redgifs.com": self.redgifs,
-                        "cyberfile.is": self.cyberfile, "coomer.party": self.coomer}
 
-    async def ShareX(self, url: URL, title=None):
-        if not self.sharex_crawler:
-            self.sharex_crawler = ShareXCrawler(include_id=self.include_id)
-        domain_obj = await self.sharex_crawler.fetch(self.session, url)
+        self.anonfiles_crawler = None
+        self.chibisafe_crawler = None
+        self.coomer_crawler = None
+        self.cyberfile_crawler = None
+        self.erome_crawler = None
+        self.gfycat_crawler = None
+        self.gofile_crawler = None
+        self.pixeldrain_crawler = None
+        self.redgifs_crawler = None
+        self.sharex_crawler = None
+        self.thotsbay_crawler = None
+
+        self.semaphore = asyncio.Semaphore(1)
+        self.mapping = {"anonfiles.com": self.Anonfiles, "bunkr.is": self.Chibisafe,
+                        "bunkr.to": self.Chibisafe, "coomer.party": self.coomer,
+                        "cyberdrop.cc": self.Chibisafe, "cyberdrop.me": self.Chibisafe,
+                        "cyberdrop.nl": self.Chibisafe, "cyberdrop.to": self.Chibisafe,
+                        "cyberfile.is": self.cyberfile, "erome.com": self.Erome,
+                        "gfycat.com": self.gfycat, "gofile.io": self.GoFile,
+                        "jpg.church": self.ShareX, "pixeldrain.com": self.Pixeldrain,
+                        "pixl.is": self.ShareX, "putme.ga": self.ShareX,
+                        "putmega.com": self.ShareX, "redgifs.com": self.redgifs,
+                        "socialmediagirls.com": self.ThotsBay, "thotsbay.com": self.ThotsBay}
+
+    async def Anonfiles(self, url: URL, title=None):
+        if not self.anonfiles_crawler:
+            self.anonfiles_crawler = AnonfilesCrawler(
+                include_id=self.include_id)
+        domain_obj = await self.anonfiles_crawler.fetch(self.session, url)
         if title:
             await domain_obj.append_title(title)
         await self.Cascade.add_albums(domain_obj)
@@ -59,6 +64,31 @@ class ScrapeMapper():
             self.chibisafe_crawler = ChibisafeCrawler(
                 include_id=self.include_id)
         domain_obj = await self.chibisafe_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def coomer(self, url: URL, title=None):
+        if not self.coomer_crawler:
+            self.coomer_crawler = CoomerCrawler(include_id=self.include_id)
+        domain_obj = await self.coomer_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def cyberfile(self, url: URL, title=None):
+        if not self.cyberfile_crawler:
+            self.cyberfile_crawler = CyberfileCrawler()
+        async with self.semaphore:
+            domain_obj = await self.cyberfile_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def Erome(self, url: URL, title=None):
+        if not self.erome_crawler:
+            self.erome_crawler = EromeCrawler(include_id=self.include_id)
+        domain_obj = await self.erome_crawler.fetch(self.session, url)
         if title:
             await domain_obj.append_title(title)
         await self.Cascade.add_albums(domain_obj)
@@ -75,38 +105,6 @@ class ScrapeMapper():
             await domain_obj.append_title(title)
         await self.Cascade.add_albums(domain_obj)
 
-    async def Anonfiles(self, url: URL, title=None):
-        if not self.anonfiles_crawler:
-            self.anonfiles_crawler = AnonfilesCrawler(
-                include_id=self.include_id)
-        domain_obj = await self.anonfiles_crawler.fetch(self.session, url)
-        if title:
-            await domain_obj.append_title(title)
-        await self.Cascade.add_albums(domain_obj)
-
-    async def Erome(self, url: URL, title=None):
-        if not self.erome_crawler:
-            self.erome_crawler = EromeCrawler(include_id=self.include_id)
-        domain_obj = await self.erome_crawler.fetch(self.session, url)
-        if title:
-            await domain_obj.append_title(title)
-        await self.Cascade.add_albums(domain_obj)
-
-    async def Pixeldrain(self, url: URL, title=None):
-        if not self.pixeldrain_crawler:
-            self.pixeldrain_crawler = PixelDrainCrawler()
-        domain_obj = await self.pixeldrain_crawler.fetch(self.session, url)
-        if title:
-            await domain_obj.append_title(title)
-        await self.Cascade.add_albums(domain_obj)
-
-    async def ThotsBay(self, url: URL, title=None):
-        if not self.thotsbay_crawler:
-            self.thotsbay_crawler = ThotsbayCrawler(
-                include_id=self.include_id, auth=self.thotsbay_auth,
-                scraping_mapper=self, session=self.session, separate_posts=self.separate_posts)
-        await self.Cascade.extend(await self.thotsbay_crawler.fetch(self.session, url))
-
     async def gfycat(self, url: URL, title=None):
         if not self.gfycat_crawler:
             self.gfycat_crawler = GfycatCrawler(
@@ -118,6 +116,14 @@ class ScrapeMapper():
             else:
                 await self.Cascade.add_to_album("gfycat.com", "gifs", content_url, url)
 
+    async def Pixeldrain(self, url: URL, title=None):
+        if not self.pixeldrain_crawler:
+            self.pixeldrain_crawler = PixelDrainCrawler()
+        domain_obj = await self.pixeldrain_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
     async def redgifs(self, url: URL, title=None):
         if not self.redgifs_crawler:
             self.redgifs_crawler = RedGifsCrawler(scraping_mapper=self, session=self.session)
@@ -128,22 +134,20 @@ class ScrapeMapper():
             else:
                 await self.Cascade.add_to_album("redgifs.com", "gifs", content_url, url)
 
-    async def cyberfile(self, url: URL, title=None):
-        if not self.cyberfile_crawler:
-            self.cyberfile_crawler = CyberfileCrawler()
-        async with self.semaphore:
-            domain_obj = await self.cyberfile_crawler.fetch(self.session, url)
+    async def ShareX(self, url: URL, title=None):
+        if not self.sharex_crawler:
+            self.sharex_crawler = ShareXCrawler(include_id=self.include_id)
+        domain_obj = await self.sharex_crawler.fetch(self.session, url)
         if title:
             await domain_obj.append_title(title)
         await self.Cascade.add_albums(domain_obj)
 
-    async def coomer(self, url: URL, title=None):
-        if not self.coomer_crawler:
-            self.coomer_crawler = CoomerCrawler(include_id=self.include_id)
-        domain_obj = await self.coomer_crawler.fetch(self.session, url)
-        if title:
-            await domain_obj.append_title(title)
-        await self.Cascade.add_albums(domain_obj)
+    async def ThotsBay(self, url: URL, title=None):
+        if not self.thotsbay_crawler:
+            self.thotsbay_crawler = ThotsbayCrawler(
+                include_id=self.include_id, auth=self.thotsbay_auth,
+                scraping_mapper=self, session=self.session, separate_posts=self.separate_posts)
+        await self.Cascade.extend(await self.thotsbay_crawler.fetch(self.session, url))
 
     async def map_url(self, url_to_map: URL, title=None):
         for key, value in self.mapping.items():
