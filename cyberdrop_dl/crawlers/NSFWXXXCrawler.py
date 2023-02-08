56136270d0a29988fb0848a623cca8d98b37b80c
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-02-08 08:20:53 -0700

    NSFW.XXX Cover gallery posts.

--- cyberdrop_dl/crawlers/NSFWXXXCrawler.py
+++ cyberdrop_dl/crawlers/NSFWXXXCrawler.py
@@ -34,18 +34,28 @@ class NSFWXXXCrawler:
 
                 posts = page_soup.select('div[class="sh-section__image grid-item"] a[class=slider_init_href]')
                 posts.extend(page_soup.select('div[class="sh-video__player"] a[class=slider_init_href]'))
+                posts.extend(page_soup.select('div[class="sh-section__images row"] div a'))
 
                 if not posts:
                     break
 
+                posts = await self.get_post_hrefs(posts)
                 for post in posts:
-                    await self.get_post(session, post.get('href'), domain_obj)
+                    await self.get_post(session, post, domain_obj)
                 page += 1
 
         except Exception as e:
             logger.debug("Error encountered while handling %s", str(url), exc_info=True)
             await log(f"[red]Error: {str(url)}[/red]", quiet=self.quiet)
             logger.debug(e)
+
+    async def get_post_hrefs(self, posts):
+        posts_links = []
+        for post in posts:
+            url = URL(post.get("href"))
+            if not url in posts_links:
+                posts_links.append(url)
+        return posts_links
 
     async def get_post(self, session: ScrapeSession, url: URL, domain_obj: DomainItem):
         try:
@@ -55,6 +65,7 @@ class NSFWXXXCrawler:
 
             content_obj = soup.select("div[class=sh-section__image] img")
             content_obj.extend(soup.select("video source"))
+            content_obj.extend(soup.select('div[class="sh-section__images sh-section__images_gallery row"] div a img'))
 
             for content in content_obj:
                 link = URL(content.get("src"))
