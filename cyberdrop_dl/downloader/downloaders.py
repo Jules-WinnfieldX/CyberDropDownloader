158224ef8a12f081bd7d29b691934b2a6c5e5285
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-06-08 00:42:05 -0600

    idk why I took out this exception suppression.

--- cyberdrop_dl/downloader/downloaders.py
+++ cyberdrop_dl/downloader/downloaders.py
@@ -253,7 +253,8 @@ class Downloader:
             if await self.CDL_Helper.File_Lock.check_lock(filename):
                 await self.CDL_Helper.File_Lock.remove_lock(filename)
 
-            await self.Progress_Master.FileProgress.remove_file(file_task)
+            with contextlib.suppress(Exception):
+                await self.Progress_Master.FileProgress.remove_file(file_task)
 
             if hasattr(e, "message"):
                 logging.debug(f"\n{media.url} ({e.message})")
