eef717476afe1591dbebb6ab1fed71fd195a6070
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-08-15 20:18:01 -0600

    Add functions / some functionality to DB

--- cyberdrop_dl/utils/db/tables/cache_table.py
+++ cyberdrop_dl/utils/db/tables/cache_table.py
@@ -6,6 +6,7 @@ from cyberdrop_dl.utils.db.table_definitions import create_cache
 class CacheTable:
     def __init__(self, db_conn: aiosqlite.Connection):
         self.db_conn: aiosqlite.Connection = db_conn
+        self.ignore_cache: bool = False
 
     async def startup(self) -> None:
         """Startup process for the CacheTable"""
