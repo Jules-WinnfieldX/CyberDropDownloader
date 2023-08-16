c3aca4b36354f589fc620edfccad54d3aa43d7cc
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-08-15 19:37:57 -0600

    Add utility functions

--- cyberdrop_dl/utils/utilities.py
+++ cyberdrop_dl/utils/utilities.py
@@ -1,9 +1,55 @@
-import asyncio
 import logging
-import os
-from pathlib import Path
+import re
+from typing import TYPE_CHECKING
 
+from cyberdrop_dl.clients.errors import NoExtensionFailure
+
+if TYPE_CHECKING:
+    from typing import Tuple
 
 logger = logging.getLogger(__name__)
 
+MAX_NAME_LENGTHS = {"FILE": 95, "FOLDER": 60}
 
+FILE_FORMATS = {
+    'Images': {
+        '.jpg', '.jpeg', '.png', '.gif',
+        '.gifv', '.webp', '.jpe', '.svg',
+        '.jfif', '.tif', '.tiff', '.jif',
+    },
+    'Videos': {
+        '.mpeg', '.avchd', '.webm', '.mpv',
+        '.swf', '.avi', '.m4p', '.wmv',
+        '.mp2', '.m4v', '.qt', '.mpe',
+        '.mp4', '.flv', '.mov', '.mpg',
+        '.ogg', '.mkv', '.mts', '.ts',
+        '.f4v'
+    },
+    'Audio': {
+        '.mp3', '.flac', '.wav', '.m4a',
+    },
+    'Text': {
+        '.htm', '.html', '.md', '.nfo',
+        '.txt',
+    }
+}
+
+
+async def sanitize(name: str) -> str:
+    """Simple sanitization to remove illegal characters"""
+    return re.sub(r'[<>:"/\\|?*\']', "", name).strip()
+
+
+async def get_filename_and_ext(filename: str, forum: bool = False) -> Tuple[str, str]:
+    """Returns the filename and extension of a given file, throws NoExtensionFailure if there is no extension"""
+    filename_parts = filename.rsplit('.', 1)
+    if len(filename_parts) == 1:
+        raise NoExtensionFailure()
+    if filename_parts[-1].isnumeric() and forum:
+        filename_parts = filename_parts[0].rsplit('-', 1)
+    ext = "." + filename_parts[-1].lower()
+    filename = filename_parts[0][:MAX_NAME_LENGTHS['FILE']] if len(filename_parts[0]) > MAX_NAME_LENGTHS['FILE'] else filename_parts[0]
+    filename = filename.strip()
+    filename = filename.rstrip(".")
+    filename = await sanitize(filename + ext)
+    return filename, ext
