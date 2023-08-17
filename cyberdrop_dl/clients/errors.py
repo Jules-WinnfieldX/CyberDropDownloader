9d7875fe11e3a3e929e935f5b1849752d9129db8
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-08-16 20:47:02 -0600

    Add downloadfailure to errors file

--- cyberdrop_dl/clients/errors.py
+++ cyberdrop_dl/clients/errors.py
@@ -1,6 +1,7 @@
 
 class InvalidContentTypeFailure(Exception):
-    def __init__(self, *, message: str = "Failed login."):
+    """This error will be thrown when the content type isn't as expected"""
+    def __init__(self, *, message: str = "Invalid content type"):
         self.message = message
         super().__init__(self.message)
 
@@ -10,3 +11,12 @@ class NoExtensionFailure(Exception):
     def __init__(self, *, message: str = "Extension missing for file"):
         self.message = message
         super().__init__(self.message)
+
+
+class DownloadFailure(Exception):
+    """This error will be thrown when a download fails"""
+    def __init__(self, status: int, message: str = "Something went wrong"):
+        self.status = status
+        self.message = message
+        super().__init__(self.message)
+        super().__init__(self.status)
