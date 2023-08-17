9fa171be05a43912a691cddf2678be6790a852fd
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-08-16 20:47:34 -0600

    re-add download_filename to history

--- cyberdrop_dl/utils/db/table_definitions.py
+++ cyberdrop_dl/utils/db/table_definitions.py
@@ -1,7 +1,7 @@
 create_history = """CREATE TABLE IF NOT EXISTS media (domain TEXT,
                                                url_path TEXT,
-                                               album_path TEXT,
                                                referer TEXT,
+                                               download_filename TEXT,
                                                original_filename TEXT,
                                                completed INTEGER NOT NULL,
                                                PRIMARY KEY (url_path, original_filename)
