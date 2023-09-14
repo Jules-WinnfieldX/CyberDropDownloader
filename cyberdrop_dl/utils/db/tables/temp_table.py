daee7d6d5b4eef2445ed5c50821ad808729966b3
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-09-13 20:02:05 -0600

    write temp table funcs

--- cyberdrop_dl/utils/db/tables/temp_table.py
+++ cyberdrop_dl/utils/db/tables/temp_table.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import aiosqlite
 
 from cyberdrop_dl.utils.db.table_definitions import create_temp
@@ -10,4 +12,17 @@ class TempTable:
     async def startup(self) -> None:
         """Startup process for the TempTable"""
         await self.db_conn.execute(create_temp)
+        await self.db_conn.commit()
+
+    async def get_temp_names(self) -> List[str]:
+        """Gets the list of temp filenames"""
+        cursor = await self.db_conn.cursor()
+        await cursor.execute("SELECT downloaded_filename FROM downloads_temp;")
+        filenames = await cursor.fetchall()
+        filenames = [list(filename) for filename in filenames]
+        return list(sum(filenames, ()))
+
+    async def sql_insert_temp(self, downloaded_filename: str) -> None:
+        """Inserts a temp filename into the downloads_temp table"""
+        await self.db_conn.execute("""INSERT OR IGNORE INTO downloads_temp VALUES (?)""", (downloaded_filename,))
         await self.db_conn.commit()
