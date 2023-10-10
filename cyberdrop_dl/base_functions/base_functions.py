d17a5890fb0455ba5ef3733247047d1b8ecfa50c
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-10-10 14:57:38 -0600

    remove emojis from filenames

--- cyberdrop_dl/base_functions/base_functions.py
+++ cyberdrop_dl/base_functions/base_functions.py
@@ -79,7 +79,18 @@ async def purge_dir(dirname: Path) -> None:
 
 async def sanitize(name: str) -> str:
     """Simple sanitization to remove illegal characters"""
-    return re.sub(r'[<>:"/\\|?*\']', "", name).strip()
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
+    illegal_char_pattern = re.compile(r'[<>:"/\\|?*\']')
+    name = emoji_pattern.sub(r'', name)
+    name = illegal_char_pattern.sub(r'', name)
+    return name
 
 
 async def make_title_safe(title: str) -> str:
