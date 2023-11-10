061c73e86de17083ccfb2abdc0ebff3c8f295f5d
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2023-11-09 22:14:03 -0700

    finish cache table functions

--- cyberdrop_dl/utils/db/tables/cache_table.py
+++ cyberdrop_dl/utils/db/tables/cache_table.py
@@ -1,6 +1,11 @@
+from typing import TYPE_CHECKING, Optional
+
 import aiosqlite
 
 from cyberdrop_dl.utils.db.table_definitions import create_cache
+
+if TYPE_CHECKING:
+    from yarl import URL
 
 
 class CacheTable:
@@ -12,3 +17,20 @@ class CacheTable:
         """Startup process for the CacheTable"""
         await self.db_conn.execute(create_cache)
         await self.db_conn.commit()
+
+    async def insert_blob(self, blob: str, url: 'URL') -> None:
+        """Inserts the post content into coomeno"""
+        await self.db_conn.execute("""INSERT OR IGNORE INTO cache VALUES (?, ?)""", (url.path, blob,))
+        await self.db_conn.commit()
+
+    async def get_blob(self, url: 'URL') -> Optional[str]:
+        """returns the post content for a given coomeno post url"""
+        if self.ignore_cache:
+            return None
+
+        cursor = await self.db_conn.cursor()
+        await cursor.execute("""SELECT post_data FROM cache WHERE url_path = ?""", (url.path,))
+        sql_file_check = await cursor.fetchone()
+        if sql_file_check:
+            return sql_file_check[0]
+        return None
