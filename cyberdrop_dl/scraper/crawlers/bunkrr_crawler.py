ba8a4cf95229dbb15d84ef1bbb27c46f471116be
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2024-03-05 15:50:19 -0700

    Fix bunkr reinforced links

--- cyberdrop_dl/scraper/crawlers/bunkrr_crawler.py
+++ cyberdrop_dl/scraper/crawlers/bunkrr_crawler.py
@@ -158,7 +158,7 @@ class BunkrrCrawler(Crawler):
         async with self.request_limiter:
             soup = await self.client.get_BS4(self.domain, url)
 
-        link_container = soup.select("a[class*=bg-blue-500]")[-1]
+        link_container = soup.select('a[download*=""]')[-1]
         link = URL(link_container.get('href'))
 
         return link
