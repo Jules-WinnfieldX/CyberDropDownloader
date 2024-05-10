a25b0729ada509baa81ccfc9c9a5447adb35b68b
Jules-WinnfieldX <juleswinnfield@labyrinth52.com>
2024-05-09 19:55:35 -0600

    change to proper head request

--- cyberdrop_dl/clients/download_client.py
+++ cyberdrop_dl/clients/download_client.py
@@ -79,8 +79,8 @@ class DownloadClient:
         headers['Referer'] = str(media_item.referer)
         headers.update(headers_inc)
 
-        async with client_session.get(media_item.url, headers=headers, ssl=self.client_manager.ssl_context,
-                                      proxy=self.client_manager.proxy) as resp:
+        async with client_session.head(media_item.url, headers=headers, ssl=self.client_manager.ssl_context,
+                                       proxy=self.client_manager.proxy) as resp:
             await self.client_manager.check_http_status(resp)
             return int(resp.headers.get('Content-Length', '0'))
 
